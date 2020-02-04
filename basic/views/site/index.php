928497d232bae2b5c226f299f8edcfaf2f245023
mmishanin <mmishanin@wowworks.ru>
2020-02-04 14:35:02 +0400

    3

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -8,6 +8,7 @@ $this->registerJsFile('scripts/script.js', ['position' => View::POS_BEGIN]);
 ?>
 
 <form action="">
+    <p>Ввод ссылки без http и www, начиная с имени домена</p>
     <label for="search" class="form_label">Ваша ссылка</label>
     <input type="text" class="search" id="search">
     <input type="submit" value="Отправить" class="submit" onClick="clickSubmit();return false">
